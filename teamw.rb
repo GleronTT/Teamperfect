03141cddb735e3cd401c3038018c9f6ea78eca98
GleronTT <gleron1995@gmail.com>
2016-01-07 21:45:39 +0200

    second upload

--- teamw.rb
+++ teamw.rb
@@ -1,6 +1,17 @@
 class Panda
+	attr_accesor :name, :email, :gander 
+
+    def intialize(name ,email , gander)
+    	@name , @email, @gander = name , email , gander 
+    end
    def to_s
-    
+     "Name: #{name}, email: #{email}, gander: #{gander}"    
+   end
+   def male?
+     gander == "male"
+   end
+   def female?
+     gander == "famale"
    end
 
    def to_hash
@@ -8,7 +19,11 @@ class Panda
 		self.each do |elem|
 			hash[elem[0]] = elem[1]
 		end
-		hash
 	end
-
+	
+	def equal?(other)
+		name == other.name && email == other.email && gender == other.gender 
+	end
 end
+
+class Panda
